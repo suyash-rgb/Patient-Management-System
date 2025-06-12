770ef4fe54606cbcaac56e9cd6f623d1e1efb1db
Suyash Baoney <suyashbaoney58@gmail.com>
2025-06-12 18:42:56 +0530

    Initial Blank Project setup for backend

---
+++ patient-management-system-backend/src/main/java/com/suyash/pms/patient_management_system_backend/PatientManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.suyash.pms.patient_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ patient-management-system-backend/src/test/java/com/suyash/pms/patient_management_system_backend/PatientManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyash.pms.patient_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
